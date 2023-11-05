ac5b61f132df9e21f3eaf649b97143e20043664d
door-yeah <dptjd0216@naver.com>
2023-11-06 00:34:16 +0900

    statefull

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -5,110 +5,68 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  //const MyApp({super.key});
-
-  bool enabled = false;
-  String stateText = "disabled";
-
-  void changeCheck() {
-    if (enabled) {
-      stateText = "disable";
-      enabled = false;
-    } else {
-      stateText = "enable";
-      enabled = true;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //   title: 'Flutter Demo',
-      //   theme: ThemeData(
-      //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      //     useMaterial3: true,
-      //   ),
-      //   home: const MyHomePage(title: 'Flutter Demo Home Page'),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Stateless Test'),
+          title: Text('Stateful Test'),
         ),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: (enabled
-                    ? Icon(
-                        Icons.check_box,
-                        size: 20,
-                      )
-                    : Icon(
-                        Icons.check_box_outline_blank,
-                        size: 20,
-                      )),
-                color: Colors.blue,
-                onPressed: changeCheck,
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 16),
-                child: Text(
-                  '$stateText',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-        ),
+        body: MyWidget(),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class MyWidget extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return _MyWidgetState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyWidgetState extends State<MyWidget> {
+  bool enabled = false;
+  String stateText = "disable";
 
-  void _incrementCounter() {
+  void changeCheck() {
     setState(() {
-      _counter++;
+      if (enabled) {
+        stateText = "disable";
+        enabled = false;
+      } else {
+        stateText = "enable";
+        enabled = true;
+      }
     });
   }
 
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            icon: (enabled
+                ? Icon(
+                    Icons.check_box,
+                    size: 20,
+                  )
+                : Icon(
+                    Icons.check_box_outline_blank,
+                    size: 20,
+                  )),
+            color: Colors.red,
+            onPressed: changeCheck,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 16),
+            child: Text(
+              '$stateText',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
